c94cd1f8444ba48ebf73e6f4a6cf8ea47c3a0a97
Ronald Main <rbmain84@yahoo.com>
2021-06-23 15:28:12 -0500

    created Comment.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -11,3 +12,24 @@ Post.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'SET NULL'
 });
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
